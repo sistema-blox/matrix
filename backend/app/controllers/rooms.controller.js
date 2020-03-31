36d2eab32e07042b6349b32b6a23fd55e098469d
Matheus Figueiredo <matheus.figueiredo@nave.rs>
2020-03-31 13:49:38 -0300

    blox config room

--- backend/app/controllers/rooms.controller.js
+++ backend/app/controllers/rooms.controller.js
@@ -25,22 +25,48 @@ const createRoomFileSync = () => {
     disableMeeting: true,
   };
 
-  const niceNames = [
-    "Nebuchadnezzar",
-    "Logos",
-    "Osiris",
-    "Icarus",
-    "Caduceus",
-    "Brahma",
-    "Novalis",
-    "Vigilant",
-    "Zion",
+  const roomNames = [
+    "Papo de Buteco",
+    "Sala de Treinamentos",
+    "Sala Comercial",
+    "Sala de Setup Pedagógico",
+    "Sala de Desenvolvimento",
+    "Sala de Produto",
   ];
 
-  for (const niceName of niceNames) {
+  const bloxRoom = [
+    {
+      header_color: "#4EB585",
+      blox_color: '#56D59A'
+    }
+    {
+      header_color: "#2196F3",
+      blox_color: '#2380C1'
+    }
+    {
+      header_color: "#EA8B68",
+      blox_color: '#D66C45'
+    }
+    {
+      header_color: "#2196F3",
+      blox_color: '#13628E'
+    }
+    {
+      header_color: "#D82E60",
+      blox_color: '#B4254F'
+    }
+    {
+      header_color: "#4D4595",
+      blox_color: '#1F39D8'
+    }
+  ]
+
+  for (const roomName of roomNames) {
     roomsData.push({
       id: uuid(),
-      name: niceName,
+      name: roomName,
+      header_color: bloxRoom.header_color,
+      blox_color: bloxRoom.blox_color,
     });
   }
 
