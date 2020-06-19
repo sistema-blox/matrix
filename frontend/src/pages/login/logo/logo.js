3ff8b8e61a633ad8b16d6c627345c2ef4c6c619f
Bruno B <34753722+bruno-berchielli@users.noreply.github.com>
2020-06-19 18:41:40 -0300

    Update logo.js

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = ({ src }) => (
   <div className="row justify-content-center align-items-center">
-    <img src={src || "/images/BloxLogo.svg"} alt="blox-logo"/ width="300px">
+    <img src={src || "/images/BloxLogo.svg"} alt="blox-logo" width="300px" />
   </div>
 );
